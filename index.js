499312ae99df58da48e561b06d8e79df3ab99e9d
Ray Zana <rayzana280@gmail.com>
2023-01-03 22:48:49 -0500

    commit tree 9b867466f6ece5b5ac47afbe0e7e7b0f2764c0bc

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+function findMatching(names, string){
+   const newList= names.filter(callBack);
+
+    function callBack(element){
+        if(element.toLowerCase() === string.toLowerCase()){
+            return true;
+        }
+    }
+    return newList 
+}
+
+function fuzzyMatch(drivers, letters){
+    const driversLetter = drivers.filter(callBackLetters);
+
+    function callBackLetters (element){
+        if(element.startsWith(letters)){
+            return true;
+        }
+    }
+
+    return driversLetter
+}
+
+function matchName(object, string){
+    const driverName = object.filter(checkName);
+
+    function checkName(element){
+        if(element.name === string){
+            return true 
+        }
+    }
+    return driverName;
+}
